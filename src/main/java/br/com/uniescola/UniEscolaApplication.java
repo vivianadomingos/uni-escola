e838212163a7d75570cffd615d4dfb9cc56a6fa6
vivia <vivia@host.docker.internal>
2022-11-27 19:45:42 -0300

    criando projeto.

---
+++ src/main/java/br/com/uniescola/UniEscolaApplication.java
@@ -0,0 +1,13 @@
+package br.com.uniescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniEscolaApplication.class, args);
+	}
+
+}
