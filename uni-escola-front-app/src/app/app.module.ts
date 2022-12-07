cb334e82f2815a13420741f07624de397c048144
LAPTOP-S1B7761P\pablo <pablo.brito@live.com>
2022-12-07 00:49:47 -0300

    alteracao no navbar

--- uni-escola-front-app/src/app/app.module.ts
+++ uni-escola-front-app/src/app/app.module.ts
@@ -7,18 +7,33 @@ import { AppComponent } from './app.component';
 import { AlunosComponent } from './alunos/alunos.component';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProfessorComponent } from './professor/professor.component';
+import { TurmaComponent } from './turma/turma.component';
+import { LeadComponent } from './lead/lead.component';
+import { CursoComponent } from './curso/curso.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlunosComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProfessorComponent,
+    TurmaComponent,
+    LeadComponent,
+    CursoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      { path: 'alunos', component: AlunosComponent }
+      { path: '', component: HomeComponent },
+      { path: 'alunos', component: AlunosComponent },
+      { path: 'professor', component: ProfessorComponent },
+      { path: 'curso', component: CursoComponent },
+      { path: 'lead', component: LeadComponent },
+      { path: 'turma', component: TurmaComponent },
     ])
   ],
   providers: [],
